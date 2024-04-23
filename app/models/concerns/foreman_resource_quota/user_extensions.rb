294d3ffec9892804c03b3567fa3dc8e224bc6570
Bastian Schmidt <schmidt@atix.de>
2024-04-23 15:40:48 +0200

    Fix many to many relation naming
    * Rename resource_quota_users to resource_quotas_users
    * Rename resource_quota_usergroups to resource_quotas_usergroups

--- app/models/concerns/foreman_resource_quota/user_extensions.rb
+++ app/models/concerns/foreman_resource_quota/user_extensions.rb
@@ -4,9 +4,9 @@ module ForemanResourceQuota
   module UserExtensions
     extend ActiveSupport::Concern
     included do
-      has_many :resource_quota_users, class_name: 'ForemanResourceQuota::ResourceQuotaUser', dependent: :destroy,
+      has_many :resource_quotas_users, class_name: 'ForemanResourceQuota::ResourceQuotaUser', dependent: :destroy,
         inverse_of: :user
-      has_many :resource_quotas, class_name: 'ForemanResourceQuota::ResourceQuota', through: :resource_quota_users
+      has_many :resource_quotas, class_name: 'ForemanResourceQuota::ResourceQuota', through: :resource_quotas_users
       attribute :resource_quota_is_optional, :boolean, default: false
 
       scoped_search relation: :resource_quotas, on: :name, complete_value: true, rename: :resource_quota
