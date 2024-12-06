04c601100bd14bf2c9567078525e2714f69ca16a
Bastian Schmidt <schmidt@atix.de>
2024-12-06 11:44:57 +0100

    Remove prefix on permissions and routes
    * Adapt registered security permissions
    * Adapt routes (view and API routes)

--- config/routes.rb
+++ config/routes.rb
@@ -2,40 +2,30 @@
 
 # rubocop: disable Metrics/BlockLength
 Rails.application.routes.draw do
-  scope :foreman_resource_quota, path: 'foreman_resource_quota' do
-    resources :resource_quotas, except: %i[show], controller: 'foreman_resource_quota/resource_quotas' do
-      collection do
-        get 'auto_complete_search'
-      end
+  resources :resource_quotas, except: %i[show] do
+    collection do
+      get 'help', action: :welcome
+      get 'auto_complete_search'
     end
   end
 
-  namespace :foreman_resource_quota do
-    resources :resource_quotas, except: %i[show] do
-      collection do
-        get 'help', action: :welcome
-        get 'auto_complete_search'
-      end
-    end
-
-    # API routes
-    namespace :api, defaults: { format: 'json' } do
-      scope '(:apiv)',
-        module: :v2,
-        defaults: { apiv: 'v2' },
-        apiv: /v1|v2/,
-        constraints: ApiConstraints.new(version: 2, default: true) do
-        resources :resource_quotas, except: %i[new edit] do
-          collection do
-            get 'auto_complete_search'
-          end
-          constraints(id: %r{[^/]+}) do
-            get 'utilization'
-            get 'missing_hosts'
-            get 'hosts'
-            get 'users'
-            get 'usergroups'
-          end
+  # API routes
+  namespace :api, defaults: { format: 'json' } do
+    scope '(:apiv)',
+      module: :v2,
+      defaults: { apiv: 'v2' },
+      apiv: /v1|v2/,
+      constraints: ApiConstraints.new(version: 2, default: true) do
+      resources :resource_quotas, except: %i[new edit] do
+        collection do
+          get 'auto_complete_search'
+        end
+        constraints(id: %r{[^/]+}) do
+          get 'utilization'
+          get 'missing_hosts'
+          get 'hosts'
+          get 'users'
+          get 'usergroups'
         end
       end
     end
