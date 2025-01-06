47b630b586fd01f751c8d4952b113f5dbe5e6f50
Nadja Heitmann <nadjah@atix.de>
2025-01-06 10:14:42 +0000

    Add API doc quota params to hosts, users and usergroups controller

---
+++ app/controllers/foreman_resource_quota/concerns/api/v2/users_controller_extensions.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module ForemanResourceQuota
+  module Concerns
+    module Api
+      module V2
+        module UsersControllerExtensions
+          extend ::Apipie::DSL::Concern
+          update_api(:create, :update) do
+            param :user, Hash do
+              param :resource_quota_ids, Array, of: :number, required: false,
+    desc: N_('Resource quota IDs to be associated with this user. ')
+              param :resource_quota_is_optional, :bool,
+                desc: N_('When set to "true", it is optional for a user to assign a quota when creating new hosts.
+                         The default value is "false".')
+            end
+          end
+        end
+      end
+    end
+  end
+end
