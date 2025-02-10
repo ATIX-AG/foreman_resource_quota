f82afa5ca65d982e9ee4f1fca60e4e2c38ca7189
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-02-10 20:06:52 +0100

    i18n - pulling from tx

--- app/assets/javascripts/foreman_resource_quota/locale/en_GB/foreman_resource_quota.js
+++ app/assets/javascripts/foreman_resource_quota/locale/en_GB/foreman_resource_quota.js
@@ -6,6 +6,7 @@
         "Project-Id-Version": "foreman_resource_quota 0.3.1",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2025-01-07 14:34+0000",
+        "Last-Translator": "Andi Chandler <andi@gowling.com>, 2025",
         "Language-Team": "English (United Kingdom) (https://app.transifex.com/foreman/teams/114/en_GB/)",
         "MIME-Version": "1.0",
         "Content-Type": "text/plain; charset=UTF-8",
@@ -17,198 +18,195 @@
         "plural_forms": "nplurals=2; plural=(n != 1);"
       },
       " ${response.response.data.error.full_messages}": [
-        ""
+        " ${response.response.data.error.full_messages}"
       ],
       "${label}": [
-        ""
+        "${label}"
       ],
       "${label} *": [
-        ""
+        "${label} *"
       ],
       "${selectedUnit.symbol}": [
-        ""
+        "${selectedUnit.symbol}"
       ],
       "Actions": [
-        ""
+        "Actions"
       ],
       "Apply": [
-        ""
+        "Apply"
       ],
       "CPU cores": [
-        ""
+        "CPU cores"
       ],
       "Click on \\\\\\\"Fetch quota utilization\\\\\\\": ": [
-        ""
+        "Click on \\\\\\\"Fetch quota utilisation\\\\\\\": "
       ],
       "Consumed resources": [
-        ""
+        "Consumed resources"
       ],
-      "Create a resource quota": [
+      "Create a Resource Quota": [
         ""
       ],
       "Create resource quota": [
+        "Create resource quota"
+      ],
+      "Define a Resource Quota here and apply it to users to guarantee a fair share of your resources.": [
         ""
       ],
       "Define the Resource Quota this host counts to.": [
-        ""
+        "Define the Resource Quota this host counts to."
       ],
       "Delete %s?": [
-        ""
+        "Delete %s?"
       ],
-      "Delete a resource quota": [
+      "Delete a Resource Quota": [
         ""
       ],
       "Description": [
-        ""
+        "Description"
       ],
-      "Description of the resource quota": [
+      "Description of the Resource Quota": [
         ""
       ],
       "Disk space (GB)": [
-        ""
+        "Disk space (GB)"
       ],
       "Edit": [
-        ""
+        "Edit"
       ],
       "Edit Resource Quota %s": [
-        ""
+        "Edit Resource Quota %s"
       ],
       "Edit: ${quotaProperties[RESOURCE_IDENTIFIER_NAME]}": [
-        ""
+        "Edit: ${quotaProperties[RESOURCE_IDENTIFIER_NAME]}"
       ],
       "Fetch quota utilization": [
-        ""
+        "Fetch quota utilisation"
       ],
       "Foreman Plug-in to manage resource usage among users.": [
-        ""
+        "Foreman Plug-in to manage resource usage among users."
       ],
       "Global Resource Quota no action": [
-        ""
+        "Global Resource Quota no action"
       ],
       "It is optional for a user to assign a quota when creating new hosts": [
+        "It is optional for a user to assign a quota when creating new hosts"
+      ],
+      "List all Resource Quotas": [
         ""
       ],
-      "List all resource quotas": [
-        ""
-      ],
-      "Make the assignment of a Resource quota, during the host creation process, optional for\\n                        everyone. If this is true, user-specific \\\"optional assignment\\\" configurations are neglected.": [
+      "Make the assignment of a Resource Quota, during the host creation process, optional for\\n                        everyone. If this is true, user-specific \\\"optional assignment\\\" configurations are neglected.": [
         ""
       ],
       "Maximum disk space in GiB": [
-        ""
+        "Maximum disk space in GiB"
       ],
       "Maximum memory in MiB": [
-        ""
+        "Maximum memory in MiB"
       ],
       "Maximum number of CPU cores": [
-        ""
+        "Maximum number of CPU cores"
       ],
       "Memory (MB)": [
-        ""
+        "Memory (MB)"
       ],
       "Name": [
-        ""
+        "Name"
       ],
-      "Name of the resource quota": [
+      "Name of the Resource Quota": [
         ""
       ],
       "New Resource Quota": [
-        ""
+        "New Resource Quota"
       ],
       "New quota - no consumption to display.": [
-        ""
+        "New quota - no consumption to display."
       ],
       "No consumption to display.": [
-        ""
+        "No consumption to display."
       ],
       "No floating point for smallest unit (${units[0].symbol}).": [
-        ""
+        "No floating point for smallest unit (${units[0].symbol})."
       ],
       "None provided": [
-        ""
+        "None provided"
       ],
       "Optional Assignment": [
-        ""
+        "Optional Assignment"
       ],
       "Properties": [
-        ""
+        "Properties"
       ],
       "Quota Limit": [
-        ""
+        "Quota Limit"
       ],
       "Quotas from user groups": [
-        ""
-      ],
-      "Refresh resource quota utilization": [
-        ""
+        "Quotas from user groups"
       ],
       "Resource Quota": [
+        "Resource Quota"
+      ],
+      "Resource Quota ID.\\n                         This field is required if the setting `resource_quota_optional_assignment` is set to false.": [
+        ""
+      ],
+      "Resource Quota IDs to be associated with this user group. ": [
+        ""
+      ],
+      "Resource Quota IDs to be associated with this user. ": [
         ""
       ],
       "Resource Quota optional assignment": [
-        ""
+        "Resource Quota optional assignment"
       ],
       "Resource Quotas": [
-        ""
+        "Resource Quotas"
       ],
-      "Resource Quotas help admins to manage hardware resources including CPUs, memory, and disk space among users or usergroups. \\\\n\\\\rDefine a Resource Quota here and apply it to users in order to guarantee a free share of your resources.": [
+      "Resource Quotas help admins to manage resources including CPUs, memory, and disk space among users or user groups.": [
         ""
       ],
       "Resource Quota|Name": [
+        "Resource Quota|Name"
+      ],
+      "Show Resource Quota": [
         ""
       ],
-      "Resource quota ID.\\n                         This field is required if the setting `resource_quota_optional_assignment` is set to false.": [
-        ""
-      ],
-      "Resource quota IDs to be associated with this user group. ": [
-        ""
-      ],
-      "Resource quota IDs to be associated with this user. ": [
-        ""
-      ],
-      "Resource quotas": [
-        ""
-      ],
-      "Show hosts of a resource quota": [
-        ""
-      ],
-      "Show resource quota": [
+      "Show hosts of a Resource Quota": [
         ""
       ],
       "Show resources could not be determined when calculating utilization": [
-        ""
+        "Show resources could not be determined when calculating utilisation"
       ],
       "Show used resources of assigned hosts": [
+        "Show used resources of assigned hosts"
+      ],
+      "Show usergroups of a Resource Quota": [
         ""
       ],
-      "Show usergroups of a resource quota": [
+      "Show users of a Resource Quota": [
         ""
       ],
-      "Show users of a resource quota": [
-        ""
-      ],
-      "Take no action when a resource quota is exceeded.": [
+      "Take no action when a Resource Quota is exceeded.": [
         ""
       ],
       "The total amount of ${resourceTitle} for this quota.": [
-        ""
+        "The total amount of ${resourceTitle} for this quota."
       ],
       "This can take some time since the resources of every host, assigned to this quota, must be requested.": [
-        ""
+        "This can take some time since the resources of every host, assigned to this quota, must be requested."
       ],
       "This group has no quotas": [
-        ""
+        "This group has no quotas"
       ],
       "Total ${resourceTitle} currently in use by all hosts assigned to this quota.": [
-        ""
+        "Total ${resourceTitle} currently in use by all hosts assigned to this quota."
       ],
-      "Update a resource quota": [
+      "Update a Resource Quota": [
         ""
       ],
       "Value must be a natural number.": [
-        ""
+        "Value must be a natural number."
       ],
-      "When set to \\\"true\\\", it is optional for a user to assign a quota when creating new hosts.\\n                         The default value is \\\"false\\\".": [
+      "When set to \\\"true\\\", it is optional for a user to assign a quota when creating new hosts.\\n                                   The default value is \\\"false\\\".": [
         ""
       ]
     }
