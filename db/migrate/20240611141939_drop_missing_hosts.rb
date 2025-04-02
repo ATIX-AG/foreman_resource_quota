b73d2995de7751f15acc3e0ac80870bab9e27c5d
Markus Bucher <bucher@atix.de>
2025-04-02 15:47:11 +0200

    Fix db-migrations should be reversible

--- db/migrate/20240611141939_drop_missing_hosts.rb
+++ db/migrate/20240611141939_drop_missing_hosts.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class DropMissingHosts < ActiveRecord::Migration[6.1]
-  def up
-    drop_table :resource_quotas_missing_hosts
+  def change
+    drop_table :resource_quotas_missing_hosts do |t|
+      t.references :resource_quota, null: false, foreign_key: { to_table: :resource_quotas }
+      t.references :missing_host, null: false, unique: true, foreign_key: { to_table: :hosts }
+      t.boolean :no_cpu_cores, default: false
+      t.boolean :no_memory_mb, default: false
+      t.boolean :no_disk_gb, default: false
+      t.timestamps
+    end
   end
 end
