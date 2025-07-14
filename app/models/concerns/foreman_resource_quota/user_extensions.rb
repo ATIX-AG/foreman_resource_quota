2bf3c710fadc7fdb9ce0119200db752bbe7273f1
Nadja Heitmann <nadjah@atix.de>
2025-07-14 11:52:24 +0000

    Fix quota assignment via usergroup

--- app/models/concerns/foreman_resource_quota/user_extensions.rb
+++ app/models/concerns/foreman_resource_quota/user_extensions.rb
@@ -3,7 +3,7 @@
 module ForemanResourceQuota
   module UserExtensions
     extend ActiveSupport::Concern
-    included do
+    included do # rubocop:disable Metrics/BlockLength
       after_create :set_unassigned_quota
 
       has_many :resource_quotas_users, class_name: 'ForemanResourceQuota::ResourceQuotaUser', dependent: :destroy,
@@ -12,6 +12,15 @@ module ForemanResourceQuota
       attribute :resource_quota_is_optional, :boolean, default: false
 
       scoped_search relation: :resource_quotas, on: :name, complete_value: true, rename: :resource_quota
+
+      def quotas_with_usergroups
+        quotas = []
+        usergroups.each do |group|
+          quotas << group.resource_quotas
+        end
+        quotas << resource_quotas
+        ForemanResourceQuota::ResourceQuota.where(id: quotas.flatten.map(&:id))
+      end
 
       def assignable_resource_quotas
         resource_quotas.assignable
